2005e4fa0f6b426d4d613eda4f9e4704de89c1c0
mrcsmchll <manchinelli.m@gmail.com>
2022-10-06 11:59:35 -0300

    progress in trab1...js

--- file-management/trabajoPractico1.js
+++ file-management/trabajoPractico1.js
@@ -1,11 +1,19 @@
 //TODO: REVISAR
 const fs = require('fs')
 // const prompt = prompt('prompt-sync')
-
+try {
+    
 let input = "./private-data.txt"
 
 let datos = fs.readFileSync(input, "utf-8")
 
 console.log(`pwnd: \n${datos}\n ┴─┴︵╰（‵□′╰）`)
 
+fs.writeFileSync(input, new Date().toLocaleDateString())
 
+datos = fs.readFileSync(input, "utf-8")
+
+console.log(datos)
+}catch (error) {
+    console.log(`======Error while reading/writing:======\n${error.message}\n\t\t============`)
+}
