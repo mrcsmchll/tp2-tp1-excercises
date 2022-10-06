aa40ac839eae90fc2edf9a555a5569b5ad02838a
mrcsmchll <manchinelli.m@gmail.com>
2022-10-06 12:34:46 -0300

    specified fs.appendFileSync()

--- file-management/trabajoPractico1.js
+++ file-management/trabajoPractico1.js
@@ -2,18 +2,21 @@
 const fs = require('fs')
 // const prompt = prompt('prompt-sync')
 try {
-    
-let input = "./private-data.txt"
 
-let datos = fs.readFileSync(input, "utf-8")
+    let inputPath = "./private-data.txt"
 
-console.log(`pwnd: \n${datos}\n ┴─┴︵╰（‵□′╰）`)
+    let datos = fs.readFileSync(inputPath, "utf-8")
 
-fs.writeFileSync(input, new Date().toLocaleDateString())
+    console.log(`pwnd?: \n${datos}\n ┴─┴︵╰（‵□′╰）`)
+    console.log(`====attempt at adding more data to text====`)
+   
+    fs.appendFileSync(inputPath,
+        `\n ${new Date().toLocaleDateString()} | ${new Date().toLocaleTimeString()}`)
 
-datos = fs.readFileSync(input, "utf-8")
+    datos = fs.readFileSync(inputPath, "utf-8")
 
-console.log(datos)
-}catch (error) {
+    console.log(`is this new data?\n${datos}`)
+
+} catch (error) {
     console.log(`======Error while reading/writing:======\n${error.message}\n\t\t============`)
 }
