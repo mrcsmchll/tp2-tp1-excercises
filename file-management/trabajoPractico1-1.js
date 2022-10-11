91a934c2548e681d34ad6d183d052c0b1287d3b6
mrcsmchll <manchinelli.m@gmail.com>
2022-10-11 18:56:43 -0300

    input through console test

--- file-management/trabajoPractico1-1.js
+++ file-management/trabajoPractico1-1.js
@@ -1,19 +1,20 @@
 //TODO: REVISAR
 const fs = require('fs')
 // const prompt = prompt('prompt-sync')
+
 try {
 
-    let inputPath = "./private-data.txt"
+    let filePath = "./private-data.txt"
 
-    let datos = fs.readFileSync(inputPath, "utf-8")
+    let datos = fs.readFileSync(filePath, "utf-8")
 
     console.log(`pwnd?: \n${datos}\n ┴─┴︵╰（‵□′╰）`)
     console.log(`====attempt at adding more data to text====`)
-   
-    fs.appendFileSync(inputPath,
-        `\n ${new Date().toLocaleDateString()} | ${new Date().toLocaleTimeString()}`)
 
-    datos = fs.readFileSync(inputPath, "utf-8")
+    fs.appendFileSync(filePath,
+        `\nAdded data: ${new Date().toLocaleDateString()} | ${new Date().toLocaleTimeString()}`)
+
+    datos = fs.readFileSync(filePath, "utf-8")
 
     console.log(`is this new data?\n${datos}`)
 
--- file-management/trabajoPractico1-2.js
+++ file-management/trabajoPractico1-2.js
@@ -1,1 +1,14 @@
 //TODO: consigna 2
+import * as readline from 'node:readline'
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question('Who are you?', name => {
+    console.log(`Hey there ${name}!`);
+    readline.close();
+  })
+
+  
