79e08c5f15affd43ce8e01b3d8f90b5ba9fae7c8
mrcsmchll <manchinelli.m@gmail.com>
2022-10-12 19:21:28 -0300

    started excercise 3

--- file-management/trabajoPractico1-3.js
+++ file-management/trabajoPractico1-3.js
@@ -1,1 +1,20 @@
 //TODO: consigna 3 ver clase3 o 4 PNT2 
+
+const transformarStringEnArrayDeNumeros = (text, charSep) => {
+    let resultado = []
+    let separatedText;
+    separatedText = text.split(charSep)
+    for (i = 0; i < text.length(); i++) {
+        
+    }
+    return resultado
+
+}
+
+
+
+let texto = "123 | 456 | 789 | 1bc | 10";
+let separador = " | "  
+
+// expected Output: [123, 456, 789, 10]
+console.log(texto.length)
