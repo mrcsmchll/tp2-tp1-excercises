694c2087f95e7315f8d746af82effb11ccd2f9c4
mrcsmchll <manchinelli.m@gmail.com>
2022-10-13 18:50:44 -0300

    small fix

--- file-management/trabajoPractico1-3.js
+++ file-management/trabajoPractico1-3.js
@@ -1,10 +1,11 @@
 //TODO: consigna 3 ver clase3 o 4 PNT2 
 
 const transformarStringEnArrayDeNumeros = (text, charSep) => {
+
     let resultado = []
     let separatedText;
     separatedText = text.split(charSep)
-    for (i = 0; i < text.length(); i++) {
+    for (i = 0; i < text.length; i++) {
         
     }
     return resultado
