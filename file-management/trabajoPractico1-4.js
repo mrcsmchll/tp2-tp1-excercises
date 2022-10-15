ed5a32ed225e0789da3ea48a7ade7f6b42213817
mrcsmchll <manchinelli.m@gmail.com>
2022-10-14 22:47:47 -0300

    excercise 4 done

--- file-management/trabajoPractico1-4.js
+++ file-management/trabajoPractico1-4.js
@@ -1,8 +1,10 @@
-// Input: array = [123, 456, 789, 10] , separador = ‘,’
+const array = [123, 456, 789, 10]
+const separador = ","
 
 
 const transformarArraysDeNumerosAUnSoloString = (array, charSeque) => {
-
+    return array.reduce((previousValue, currentValue) => previousValue + charSeque + currentValue)
 }
 
+console.log(transformarArraysDeNumerosAUnSoloString(array, separador))
 //Expected Output: ‘123,456,789,10’
